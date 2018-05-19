9830f87ec3ac169425720b6efdf7cbba40efd6e5
DevinPierce <devinmpierce@gmail.com>
2018-05-19 15:21:08 -0400

    Done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
 def alphabetize(arr)
-  # code here
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by! do |string|
+    string.split('').map do |c|
+      esperanto_alphabet.index(c)
+    end
+  end
 end
